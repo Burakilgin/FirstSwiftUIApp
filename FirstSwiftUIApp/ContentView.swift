120b77230455f2cdd19e77c60fd853bffdceabb2
Burak Ilgin <burakilginnn@gmail.com>
2025-11-04 22:32:25 +0300

    Initial Commit

---
+++ FirstSwiftUIApp/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  FirstSwiftUIApp
+//
+//  Created by Burak Ilgın on 16.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            Text("What is Football to you?")
+                .scaledToFit()
+                .foregroundColor(.green)
+                .font(.largeTitle)
+                .fontWeight(.thin)
+            
+        }
+        HStack {
+         
+            Image(systemName: "figure.american.football")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.blue)
+            Image(systemName: "figure.australian.football")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.indigo)
+            Image(systemName: "figure.indoor.soccer")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.purple)
+
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirstSwiftUIApp/FirstSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIAppApp.swift
+//  FirstSwiftUIApp
+//
+//  Created by Burak Ilgın on 16.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
